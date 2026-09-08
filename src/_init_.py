036dd6c33b8990aa97769c55c94d73eac501c013
awebdev <iman123ahmadi@outlook.com>
2026-09-08 18:02:08 -0400

    Updated _init_.py
    actually added stuff inside of it

---
+++ src/_init_.py
@@ -0,0 +1,17 @@
+from .board import Board
+from .moves import Move
+from .pieces import Piece, Pawn, Knight, Bishop, Rook, Queen, King
+
+__version__ = "0.1.3"
+
+__all__ = [
+    "Board",
+    "Move",
+    "Piece",
+    "Pawn",
+    "Knight",
+    "Bishop",
+    "Rook",
+    "Queen",
+    "King",
+]
